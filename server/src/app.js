cd7b6100f626defe66d8025f95d9d4fee77721b1
Lorena Carla <lorenamelor@gmail.com>
2019-05-26 19:31:36 -0300

    permissão de cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,22 +6,29 @@ const mongoose = require('mongoose')
 class Startup {
     constructor() {
         this.express = express();
-        
+
         this.connectDatabase();
 
         this.middlewares();
         this.routes();
     }
-    connectDatabase(){
-        mongoose.connect('mongodb+srv://entechapi:oVYXzsQvxJ22Znwn@cluster0-0v7e8.azure.mongodb.net/entech?retryWrites=true', {useNewUrlParser: true});
+    connectDatabase() {
+        mongoose.connect('mongodb+srv://entechapi:oVYXzsQvxJ22Znwn@cluster0-0v7e8.azure.mongodb.net/entech?retryWrites=true', { useNewUrlParser: true });
     }
-    
+
 
     middlewares() {
         this.express.use(express.json());
         this.express.use(express.urlencoded({
             extended: false
         }))
+        this.express.use(function (req, res, next) {
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Credentials', true);
+            next();
+        });
         this.requireArchives('middlewares', this.express);
     }
 
@@ -31,7 +38,7 @@ class Startup {
 
     requireArchives(pasta, app) {
         require("fs").readdirSync(require('path').join(__dirname, pasta)).forEach(function (file) {
-            app.use(require('./' + pasta + '/' + file.replace('.js',"")))
+            app.use(require('./' + pasta + '/' + file.replace('.js', "")))
         });
     }
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-var porta =process.env.PORT || 3000
+var porta =process.env.PORT || 8000
 
 app.listen(porta, ()=> {console.log(`Escutando na porta ${porta}`)});
