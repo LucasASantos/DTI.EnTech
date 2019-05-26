5bb63b212f4a7ad9cd05aa2622a226245c42c438
Lucas Santos <lucas.santtos@hotmail.com>
2019-05-26 10:26:16 -0300

    Init da pasta middware

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, ()=> {console.log('Escutando na porta 3000')});
+var porta =process.env.PORT || 3000
+
+app.listen(porta, ()=> {console.log(`Escutando na porta ${porta}`)});
