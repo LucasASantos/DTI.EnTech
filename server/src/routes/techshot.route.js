260a0eaa9c20c1de054c554306a557d222861383
Lucas Santos <lucas.santtos@hotmail.com>
2019-05-26 21:38:05 -0300

    Correção na rota de post

--- server/src/routes/techshot.route.js
+++ server/src/routes/techshot.route.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/techshot.controller')
 
 route.get('/techshot/:surveyId', controller.listTechshotBySurveyId);
 route.get('/techshot/:id', controller.getTechshot);
-route.post('/techshot', controller.createTecchshot);
+route.post('/techshot', controller.createTechshot);
 route.put('/techshot/:id', controller.updateTechshot);
 route.delete('/techshot/:id', controller.deleteTechshot);
 
