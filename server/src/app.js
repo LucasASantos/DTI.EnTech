6e6c8479e40e43278c21a580d1f4ba7a32ee7a67
Lucas Santos <lucas.santtos@hotmail.com>
2019-06-10 20:34:12 -0300

    Alteração cors

--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,7 @@ class Startup {
         this.express.use(function (req, res, next) {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Headers', '*');
             res.setHeader('Access-Control-Allow-Credentials', true);
             next();
         });
